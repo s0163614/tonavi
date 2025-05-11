4c14d891f385e4fe2739ecfb70eafdc048901f1d
s0163614 <90596569+s0163614@users.noreply.github.com>
2025-05-11 22:02:22 +0300

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -22,3 +22,52 @@ CREATE TABLE orders (
     FOREIGN KEY (seller_id) REFERENCES users(id),
     FOREIGN KEY (supplier_id) REFERENCES users(id)
 );
+
+CREATE TABLE chats (
+    id VARCHAR(36) PRIMARY KEY,
+    seller_id VARCHAR(36) NOT NULL,
+    supplier_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (seller_id) REFERENCES users(id),
+    FOREIGN KEY (supplier_id) REFERENCES users(id)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE messages (
+    id VARCHAR(36) PRIMARY KEY,
+    chat_id VARCHAR(36) NOT NULL,
+    sender_id VARCHAR(36) NOT NULL,
+    content TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chats(id),
+    FOREIGN KEY (sender_id) REFERENCES users(id)
+) ENGINE=InnoDB;
+
+-- Триггер для chats
+DELIMITER $$
+CREATE TRIGGER before_chats_insert
+BEFORE INSERT ON chats
+FOR EACH ROW
+BEGIN
+    IF NEW.id IS NULL THEN
+        SET NEW.id = UUID();
+    END IF;
+END$$
+DELIMITER ;
+
+-- Триггер для messages
+DELIMITER $$
+CREATE TRIGGER before_messages_insert
+BEFORE INSERT ON messages
+FOR EACH ROW
+BEGIN
+    IF NEW.id IS NULL THEN
+        SET NEW.id = UUID();
+    END IF;
+END$$
+DELIMITER ;
+
+
+
+ALTER TABLE messages 
+ADD COLUMN is_read BOOLEAN DEFAULT FALSE;
